661dd23a6306b34925b7c813346df40b72b84485
Imtious-coder <iarafat386@gmail.com>
2021-09-06 21:03:25 +0600

    Details Page Complete

--- src/Components/NewsDetails/NewsDetails.js
+++ src/Components/NewsDetails/NewsDetails.js
@@ -1,13 +1,23 @@
-import React from 'react';
 import { useParams } from 'react-router';
 import './NewsDetails.css';
 import Footer from '../Home/Footer/Footer'
+import NewsData from '../News.json';
 
 const NewsDetails = () => {
     const {newsId} = useParams()
+    const News = NewsData.find(news => news.id == newsId);
     return (
         <section>
-            This is lol{newsId}
+            <section className="grid md:grid-cols-2 mx-10">
+                {/* image */}
+               <img className="NewsDeatailsImage" src={News.image} alt="" />
+               {/* details */}
+               <div className="NewsDeatailsInfo">
+                   <p className="NewsDeatailsInfoTitle">{News.title}</p>
+                   <p className="NewsDeatailsInfoAuthor">{News.author}</p>
+                   <p className="NewsDeatailsInfoDescription">{News.description}</p>
+               </div>
+            </section>
             <Footer />
         </section>
     );
